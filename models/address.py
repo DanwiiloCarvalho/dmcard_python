a6f8cd1129c2bd3e29f33fe05cfbed230a8ffe52
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-09-17 22:42:26 -0300

    fix: correct Address relationship to allow optional list of Users

--- models/address.py
+++ models/address.py
@@ -3,11 +3,7 @@ from core.settings import settings as stt
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, DateTime, String
 from sqlalchemy.sql import func
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from models.user import User
+from models.user import User
 
 
 class Address(stt.DBBaseModel):
@@ -34,8 +30,9 @@ class Address(stt.DBBaseModel):
     updated_at: Mapped[datetime] = mapped_column(
         DateTime, server_default=func.now(), onupdate=func.now())
 
-    users: Mapped[list['User']] = relationship(
-        back_populates='address', lazy='joined')
+    users: Mapped[list['User'] | None] = relationship(
+        back_populates='address',
+        lazy='joined')
 
     def __repr__(self):
         return f'Nome usuário: {self.name}\nRenda: {self.income}'
