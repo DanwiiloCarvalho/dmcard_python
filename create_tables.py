f88d2cb680b66214d0c0f86bd540177ffb35315e
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-09-24 15:38:54 -0300

    refactor: add initial values to Status table during table creation

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,8 @@
 from core.database import async_engine, settings
+from core.deps import get_session
 import asyncio
+
+from models.status import Status
 
 
 async def create_tables() -> None:
@@ -8,6 +11,18 @@ async def create_tables() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
         await conn.run_sync(settings.DBBaseModel.metadata.create_all)
+
+    async for session in get_session():
+        status = [
+            Status(value="Reprovado"),
+            Status(value="Aprovado com limite fixo"),
+            Status(value="Aprovado com limite proporcional"),
+            Status(value="Aprovado com limite elevado"),
+            Status(value="Aprovado sem limite real")
+        ]
+
+        session.add_all(status)
+        await session.commit()
 
 if __name__ == '__main__':
     asyncio.run(create_tables())
