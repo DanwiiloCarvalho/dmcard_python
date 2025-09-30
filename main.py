fc8ebe3bd064114d6b0e16aa5b000480ac4cb0f4
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-09-30 18:28:41 -0300

    refactor: comment out the main execution block in main.py

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ async def db_unavailable_middleware(request: Request, call_next):
             }
         )
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app='main:app', host='0.0.0.0', port=8000, reload=True)
+# if __name__ == '__main__':
+#     import uvicorn
+#     uvicorn.run(app='main:app', host='0.0.0.0', port=8000, reload=True)
